ef92128781c06ae4c136425a137d6c40c9aa70a8
ishuin <ishu.kumars@gmail.com>
2021-07-09 17:34:50 +0530

    filtered script and made more dynamic and removed more self variables

--- available/consumers.py
+++ available/consumers.py
@@ -2,11 +2,9 @@ import requests
 import datetime
 import json
 import time
-import asyncio
-from asgiref.sync import async_to_sync, sync_to_async
-from channels.consumer import SyncConsumer, AsyncConsumer
+from asgiref.sync import sync_to_async
+from channels.consumer import AsyncConsumer
 from channels.exceptions import StopConsumer
-from find_availability import AvailableSlots
 
 VACCINE_AVAILABILITY_GROUP = "available_slots"
 
@@ -24,13 +22,14 @@ class SlotsSyncConsumer(AsyncConsumer):
 
     async def websocket_receive(self, event):
         print("receive")
-        self.today = datetime.datetime.now().date()
-        self.dates = ['-'.join(str(self.today + datetime.timedelta(days=i)).split('-')[::-1]) for i in range(1, 8)]
-        self.district_id = event.get('text', None)
+        scope_headers = {x[0].decode("utf-8"):x[1].decode("utf-8") for x in self.scope['headers']}
+        today = datetime.datetime.now().date()
+        dates = ['-'.join(str(today + datetime.timedelta(days=i)).split('-')[::-1]) for i in range(1, 8)]
+        district_id = event.get('text', None)
         while True:
-            for date in self.dates:
+            for date in dates:
                 time.sleep(3)
-                list_of_dicts = sync_to_async(self.slot_data)(date)
+                list_of_dicts = sync_to_async(self.slot_data)(date, district_id, scope_headers)
                 dicts = await list_of_dicts
                 await self.send(
                     {
@@ -48,11 +47,10 @@ class SlotsSyncConsumer(AsyncConsumer):
                             }
                         )
 
-
     async def websocket_disconnect(self, event):
         # Leave ticks group
-        raise StopConsumer()
         print("disconnected")
+        raise StopConsumer()
 
     def slots(self, event):
         print("slots function")
@@ -61,25 +59,26 @@ class SlotsSyncConsumer(AsyncConsumer):
             'text': event['content'],
         })
 
-    def slot_data(self, date):
-        self.URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict"
+    def slot_data(self, date, district_id, scope_headers):
+        url = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict"
         headers = {
-            "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-            "accept-language": 'en-US,en;q=0.9',
-            "user-agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.93 Safari/537.36 Edg/90.0.818.56',
+            "accept": 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,'
+            + 'image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+            "accept-language": scope_headers['accept-language'],
+            "user-agent": scope_headers['user-agent'],
         }
-        PARAMS = {
-            'district_id': self.district_id,
+        params = {
+            'district_id': district_id,
             'date': date
         }
-        r = self.response(self.URL, PARAMS, headers)
+        r = self.response(url, params, headers)
         list_of_dicts = json.loads(r.content.decode())['sessions']
         return list_of_dicts
 
-    def response(self, URL, PARAMS, headers):
-        self.resp = requests.get(url=URL, params=PARAMS, headers=headers)
-        if self.resp.status_code != 200:
+    def response(self, url, params, headers):
+        resp = requests.get(url=url, params=params, headers=headers)
+        if resp.status_code != 200:
             time.sleep(30)
             print("waiting........")
-            self.response(URL, PARAMS, headers)
-        return self.resp
+            self.response(url, params, headers)
+        return resp
